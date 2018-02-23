d16e47104eccaae127ce8cc00fd6670d7f34b365
PendulumBot <daniel.walke.r1991@gmail.com>
2018-02-23 18:05:17 +0000

    Updated bot.py with some comments yo

--- bot.py
+++ bot.py
@@ -1,3 +1,12 @@
+# Pendulum Bot by Danny Walker, 2018
+# http://danny.makesthings.work || https://walkerdanny.github.io
+# CC BY-NC-SA 4.0
+# Soon to be a major motion picture
+# See https://github.com/walkerdanny/Pendulum_Bowl for the non-bot code and explanation
+
+# Runs once a day, eventually as a cron job but at the moment it's manual.
+
+# Import that good good
 import numpy as np
 import datetime
 import subprocess
@@ -9,7 +18,7 @@ def velMag(theta_v_input, phi_v_input): # Function to calculate the magnitude of
     mag = np.sqrt(theta_v_input*theta_v_input + phi_v_input*phi_v_input)
     return mag
 
-def dist2Bottom (x_in, y_in, z_in):
+def dist2Bottom (x_in, y_in, z_in): # Calculates the distance between a point and the bottom of the "bowl"
     global L
     dist = np.sqrt(x_in**2 + y_in**2 + (z_in+L)**2)
     return dist
@@ -37,7 +46,6 @@ def update_bob(): # Iteration function to update the data array for each timeste
     theta_a = (np.sin(np.radians(theta))*np.cos(np.radians(theta))*phi_v*phi_v) - ((g/L)*np.sin(np.radians(theta)))
 
     # This line is commented out because of... reasons.
-
     #phi_a = -2*phi_v*theta_v*(np.cos(np.radians(theta))/np.sin(np.radians(theta)))
     # If you actually implement the acceleration in phi, bear in mind it tends to infinity as theta approaches zero, and you should deal with this.
     # My solution is to ignore it and pretend phi_v is constant
@@ -64,7 +72,7 @@ def update_bob(): # Iteration function to update the data array for each timeste
 
     return data
 
-def frange(start, stop, step):
+def frange(start, stop, step): # Range of floats, which apparently I do have to do myself?!
     arr = []
     i = start
     while i < stop:
@@ -103,6 +111,7 @@ data = [theta_in, theta_v_in, theta_a_in, phi_in, phi_v_in, phi_a_in, xPos_in, y
 
 initialConditionsString = "Minimum Radius: " + str(minRad) + "\nMaximum Radius: " + str(maxRad) + "\nLength: " + str(L) + "\nGravity: " + str(g) + "\nFriction: " + str(friction) + "\nPhi Velocity: " + str(phi_v_in) + "\n"
 print initialConditionsString
+
 # Set up a new array to store the traced points (with an extra column for velocity)
 thePointz = np.zeros((num_points,4))
 
@@ -111,12 +120,12 @@ for i in xrange(num_points):
     data = update_bob()
     thePointz[i] = [data[6], data[7], -data[8], velMag(data[1], data[4])]
 
-
 maxVel = np.amax(thePointz[:,3])
 minVel = np.amin(thePointz[:,3])
 
 drawBase = False;
 openscad_string = '/*\n' + initialConditionsString + '*/\n'
+
 for i in xrange(num_points-1):
     thisPoint = thePointz[i]
     nextPoint = thePointz[i+1]
@@ -129,6 +138,7 @@ for i in xrange(num_points-1):
     r2 = linear_scale(nextPoint[3], minVel, maxVel, minRad, maxRad)
 
     # Eugh...
+    # TO DO: This, but better.
     openscad_string += 'hull($fn=%(fn)f){\n' % {'fn':fn}
     openscad_string += '\ttranslate([%(xZero)f, %(yZero)f, %(zZero)f]){\n' % {"xZero":thisPoint[0], "yZero": thisPoint[1], "zZero": thisPoint[2]}
     openscad_string += '\t\tsphere(%(rad)f, $fn=%(fn)f);\n' % {'rad':r1, 'fn':fn}
@@ -147,10 +157,8 @@ if drawBase:
 
 fileName = str(datetime.datetime.now().strftime("%Y-%m-%d"))
 print fileName
+
 cadFileName = fileName + ".scad"
-
-#openscad_string = "cube([50,50,10]);"
-
 cadFile = open(cadFileName,'w')
 cadFile.write(openscad_string)
 cadFile.close()
@@ -162,37 +170,31 @@ cadFile.close()
 
 # All this shit needs porting to Unix
 
-cameraArg = "--camera=0,0,%(transZ)f,55,0,25,%(zoomOut)f" % {"transZ":-L/2,"zoomOut": L*5}
+cameraArg = "--camera=0,0,%(transZ)f,55,0,25,%(zoomOut)f" % {"transZ":-L/2,"zoomOut": L*5} # argument for rendering an image
 lastArg = "-o" + fileName + ".png"
 
+# Pick a random colour scheme for some variety
 colorschemes = ["Cornfield","Sunset","Metallic","Starnight","BeforeDawn","Nature","DeepOcean"]
 colorschemeArg = "--colorscheme=" + random.choice(colorschemes)
 
+# Generate an image from the "thrown-together" preview in OpenSCAD
 print "Generating image..."
-picName = fileName + '.png' # I have no idea why the file name has a space in front of it...
+picName = fileName + '.png'
 subprocess.check_output(["C:\Program Files\OpenSCAD\openscad.com", cadFileName, "--imgsize=1024,512", "--autocenter",colorschemeArg,"--projection=p", cameraArg, lastArg], shell=True)
 print "Generated image: " + picName
 
-
+# Generate the STL, takes AAAAAGGGEEEEESSSS
 print "Generating STL..."
 lastArg = "-o" + fileName + ".stl"
 subprocess.check_output(["C:\Program Files\OpenSCAD\openscad.com", cadFileName, lastArg], shell=True) # Get the STL in about an hours time..
 print "STL generated: " + fileName + ".stl"
-
-commitArg = "-m \"Added " + fileName + ".stl and .scad\""
-
-addArg1 = "add " + cadFileName
-addArg2 = "add " + fileName + ".stl"
-
-print addArg1
-print addArg2
 
 # Git it good
 subprocess.check_output(['git', 'add', '-A'], shell=True)
 subprocess.check_output(['git', 'commit', '-m \"Added ' + fileName + '.stl and .scad\"' ], shell=True)
 subprocess.check_output(['git', 'push'], shell=True)
 
-# Twitter shit
+# Do a tweet
 auth = tweepy.OAuthHandler(config.api_key, config.api_secret)
 auth.set_access_token(config.access_token, config.access_token_secret)
 twitter = tweepy.API(auth)
